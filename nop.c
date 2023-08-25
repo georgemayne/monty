1c4672b2a93e847e0be3e246d8561b1bea029c03
georgemayne <georgeegyirmayne07@gmail.com>
2023-08-24 19:08:53 -0700

    C - Stacks, Queues - LIFO, FIFO

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  *f_nop- function nothing
+  *@head: stack head
+  *@counter: line number
+  *Return: no return
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
